65cf7b1097f4d53bfe8528cb751929ab2bbaac8f
Mateus Dyonisio <mateeusdyonisio@gmail.com>
2021-04-26 01:39:47 -0300

    Updated Screenshot

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,17 +1,28 @@
-const playwright = require("playwright-aws-lambda")
+import chromium from 'chrome-aws-lambda';
+import playwright from 'playwright-core';
 
 module.exports = async (req, res) => {
     let browser = null
     const { query } = req
     try {
         if (query.url && isValidUrl(query.url)) {
-            browser = await playwright.launchChromium({ headless: true })
-            const context = await browser.newContext()
-            const page = await context.newPage()
-            await page.goto(query.url)
-            const screenshot = await page.screenshot({ type: "png" })
-            res.setHeader("Content-Type", "image/png")
-            res.status(200).send(screenshot)
+            browser = await playwright.chromium.launch({ 
+                args: chromium.args,
+                executablePath: await chromium.executablePath,
+                headless: chromium.headless
+        })
+
+        const page = await browser.newPage()
+        await page.goto(query.url, {
+            timeout: 15*1000
+        })
+
+        const screenshot = await page.screenshot({ 
+            type: "png" 
+        })
+
+        res.setHeader("Content-Type", "image/png")
+        res.status(200).send(screenshot)
         } else throw "Please provide a valid url"
     } catch (error) {
         res.status(500).send({
